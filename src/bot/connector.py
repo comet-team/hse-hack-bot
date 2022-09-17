09ab6bfc57dea4ccdc64db76a1ef806c81a7c30a
Komershan <74444854+Komershan@users.noreply.github.com>
2022-09-18 01:03:44 +0300

    feat: add invites (#1)
    * Автоматическая выдача админки
    
    * fear: add invites
    
    * feat: add multiple invitation

--- src/bot/connector.py
+++ src/bot/connector.py
@@ -3,14 +3,14 @@ import random
 
 class Connector:
     def get_chat_id(self):
-        return -1001762730288
+        return -1001578365228
 
     def get_chat_name(self):
         return random.choice(["Lorem", "ipsum", "dolor", "sit", "amet"])
 
     def get_chat_members(self):
-        return self.get_chat_id, ["1064231416"]
+        return self.get_chat_id, ["838662539"]
 
     def get_admin(self):
-        return "750120380"
+        return "838662539"
         # return '1064231416'
--- src/bot/tgbot.py
+++ src/bot/tgbot.py
@@ -8,6 +8,7 @@ TOKEN = os.environ["BOT_TOKEN"]
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 connector = Connector()
+invited_admins = set()
 
 # TODO давать админские права
 
@@ -25,8 +26,31 @@ async def create_chat(msg: types.Message):
 
     admin_id = connector.get_admin()
     await bot.send_message(admin_id, link.invite_link)
-    await chat.promote(admin_id, can_change_info=True, can_pin_messages=True)
 
+async def add_members(chat_id, members):
+    chat = types.Chat()
+    chat.id = connector.get_chat_id()
+    link = await chat.create_invite_link()
+
+    for member_id in members:
+        await bot.send_message(member_id, link.invite_link)
+
+async def add_admin(chat_id, admin):
+    chat = types.Chat()
+    chat.id = connector.get_chat_id()
+    link = await chat.create_invite_link()
+
+    bot.send_message(admin, link.invite_link)
+    invited_admins.add(admin)
+
+@dp.message_handler(content_types=['new_chat_members'])
+async def new_user_joined(message: types.Message):
+    chat = types.Chat()
+    chat.id = connector.get_chat_id()
+    for new_member in message.new_chat_members:
+        if new_member.id in invited_admins:
+            await chat.promote(new_member.id, can_change_info=True, can_pin_messages=True)
+            invited_admins.remove(new_member.id)
 
 def start_bot():
     executor.start_polling(dp)
