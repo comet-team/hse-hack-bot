ea01e827f6c47bf6392c9ce6164d36fd999ef0fc
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-09-18 03:58:11 +0300

    fix: add error handling

---
+++ src/utils/try_dec.py
@@ -0,0 +1,13 @@
+from functools import wraps
+import aiogram.utils.exceptions
+
+def try_dec():
+    def decorator(func):
+        @wraps(func)
+        async def result(*args: tuple, **kwargs: dict) -> None:
+            try:
+                return await func(*args, **kwargs)
+            except Exception:
+                pass
+        return result
+    return decorator
